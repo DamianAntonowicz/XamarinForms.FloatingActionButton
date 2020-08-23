70aa73fac4a7ce717a8b39429d094a27299197f0
Damian Antonowicz <kontakt@damianantonowicz.pl>
2020-08-23 19:50:57 +0200

    Added initializing FormsMaterial.

--- XamarinForms.FloatingButton.Android/MainActivity.cs
+++ XamarinForms.FloatingButton.Android/MainActivity.cs
@@ -21,6 +21,7 @@ namespace XamarinForms.FloatingButton.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            global::Xamarin.Forms.FormsMaterial.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
