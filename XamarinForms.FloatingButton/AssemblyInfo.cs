a6d027821cb9909d12bab5c80768eb1f99834ba1
Damian Antonowicz <kontakt@damianantonowicz.pl>
2020-08-23 20:35:51 +0200

    Added FontAwesome and used Plus glyph for floating button.

--- XamarinForms.FloatingButton/AssemblyInfo.cs
+++ XamarinForms.FloatingButton/AssemblyInfo.cs
@@ -1,3 +1,5 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("fa-solid-900.ttf", Alias = "FontAwesomeSolid")]
