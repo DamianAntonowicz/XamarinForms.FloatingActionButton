16e5aeeb2996ccd26df6ac47adba11462a217a5f
Damian Antonowicz <kontakt@damianantonowicz.pl>
2020-08-25 23:36:49 +0200

    Created FloatingButton component.

---
+++ XamarinForms.FloatingButton/ElementExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinForms
+{
+    public static class ElementExtensions
+    {
+        public static async Task<TParent> GetParentAsync<TParent>(this Element element)
+            where TParent : Element
+        {
+            if (element is TParent)
+            {
+                return (TParent)element;
+            }
+
+            do
+            {
+                if (element.Parent != null)
+                {
+                    element = element.Parent;
+                    continue;
+                }
+
+                var semaphore = new SemaphoreSlim(initialCount: 0);
+
+                void OnElementPropertyChanged(object sender, PropertyChangedEventArgs args)
+                {
+                    if (args.PropertyName == nameof(element.Parent) &&
+                        element.Parent != null)
+                    {
+                        semaphore.Release();
+                        element.PropertyChanged -= OnElementPropertyChanged;
+                    }
+                }
+
+                element.PropertyChanged += OnElementPropertyChanged;
+
+                await semaphore.WaitAsync();
+            } while (!(element is TParent));
+
+            return (TParent) element;
+        }
+    }
+}
---
+++ XamarinForms.FloatingButton/FloatingButton.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace XamarinForms.FloatingButton
+{
+    public partial class FloatingButton : ContentView
+    {
+        #region CommandProperty
+
+        public static readonly BindableProperty CommandProperty =
+            BindableProperty.Create(
+                propertyName: nameof(Command),
+                returnType: typeof(ICommand),
+                declaringType: typeof(FloatingButton));
+
+        public ICommand Command
+        {
+            get => (ICommand) GetValue(CommandProperty);
+            set => SetValue(CommandProperty, value);
+        }
+
+        #endregion
+        
+        #region TextColorProperty
+
+        public static readonly BindableProperty TextColorProperty =
+            BindableProperty.Create(
+                propertyName: nameof(TextColor),
+                returnType: typeof(Color),
+                declaringType: typeof(FloatingButton),
+                defaultValue: Color.White);
+
+        public Color TextColor
+        {
+            get => (Color) GetValue(TextColorProperty);
+            set => SetValue(TextColorProperty, value);
+        }
+        
+        #endregion
+        
+        #region BackgroundColorProperty
+
+        public new static readonly BindableProperty BackgroundColorProperty =
+            BindableProperty.Create(
+                propertyName: nameof(BackgroundColor),
+                returnType: typeof(Color),
+                declaringType: typeof(FloatingButton),
+                defaultValue: Color.Black);
+
+        public new Color BackgroundColor
+        {
+            get => (Color) GetValue(BackgroundColorProperty);
+            set => SetValue(BackgroundColorProperty, value);
+        }
+        
+        #endregion
+
+        public event EventHandler Clicked; 
+        
+        public FloatingButton()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnParentSet()
+        {
+            base.OnParentSet();
+            
+            var page = await this.GetParentAsync<Page>();
+
+            page.Appearing += Page_Appearing;
+            page.Disappearing += Page_Disappearing;
+        }
+
+        private void Page_Appearing(object sender, EventArgs e)
+        {
+            Button.Scale = 0;
+            Button.ScaleTo(1, easing: Easing.SpringOut);
+        }
+
+        private void Page_Disappearing(object sender, EventArgs e)
+        {
+            // Button.Scale = 0;
+            // sprawdzic na Androidzie WordPress jak wyglada animacja FloatingButtona przy wychodzeniu z widoku
+            Button.ScaleTo(0, easing: Easing.SpringOut);
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            Clicked?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
